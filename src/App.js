643d3fe62ad7ca2d13fbb27ce9e58b3891f601e2
angadvirk <angad.virk@gmail.com>
2022-10-12 15:50:09 -0700

    Using HashRouter instead of BrowserRouter as a temp solution

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Article from "./pages/Article";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
 export default function App() {
 
   return (
     <div className="app">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/article" element={<Article />} />
           {/* TODO: create '/articles/name' route */}
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
